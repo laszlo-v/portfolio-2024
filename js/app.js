8b4e24ca7d1e4ac140e8dd2f38e57b579067c6c0
laszlo-v <109346622+laszlo-v@users.noreply.github.com>
2023-09-24 08:52:11 +0100

    adding js that manages nav-log shrink

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+"use strict";
+const navH1 = document.querySelector(".nav-h1");
 
+const handleScroll = () => {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 80) {
+      navH1.classList.add("nav-h1-shrink");
+    } else {
+      navH1.classList.remove("nav-h1-shrink");
+    }
+  });
+};
+navH1 ? handleScroll() : "";
