dc5b405d1218a15332062f1de9a1957adfa9ad43
laszlo-v <109346622+laszlo-v@users.noreply.github.com>
2023-09-29 13:34:35 +0100

    img srcset, email icon

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
 "use strict";
-const navH1 = document.querySelector(".nav-h1");
 
+const navH1 = document.querySelector(".nav-h1");
+const emailIcon = document.querySelector(".email-icon");
 const handleScroll = () => {
   window.addEventListener("scroll", () => {
     if (window.scrollY > 80) {
       navH1.classList.add("nav-h1-shrink");
+      emailIcon.classList.add("email-icon-shrink");
     } else {
       navH1.classList.remove("nav-h1-shrink");
+      emailIcon.classList.remove("email-icon-shrink");
     }
   });
 };
 navH1 ? handleScroll() : "";
+
+const inputs = document.querySelectorAll(".input");
+const labels = document.querySelectorAll(".label");
+
+for (let i = 0; i < inputs.length; i++) {
+  const input = inputs[i];
+  const label = labels[i];
+
+  input.addEventListener("input", function () {
+    input.value.length
+      ? label.classList.add("shrink")
+      : label.classList.remove("shrink");
+  });
+}
